6cb142df3b872c535a15ae48fd11c7542c82c2ad
up201505509 <up201505509@fe.up.pt>
2019-04-02 23:38:53 +0100

    AsyncQueue done. AsyncSemaphore logic is there, I think

--- src/main.ts
+++ src/main.ts
@@ -1,122 +1,61 @@
-import { readFileSync, fstat } from 'fs'
+class AQSubscriber<T> {
+    constructor(public name: string) {}
 
-interface Filter {
-    next(): Message
-    hasNext(): Boolean
-}
-
-class Message {
-    constructor(public readonly value: any) { }
-    static none = new Message(null)
-}
-
-
-class AsyncQueue {
-       
-}
-
-// class Concatenate implements Filter{
-//     constructor(public readonly a: Filter, public readonly b: Filter) { }
-
-//     do(): Message {
-//         return new Message(this.a.do().value.toString() + this.b.do().value.toString())
-//     }
-// }
-
-// class ConstantString implements Filter {
-//     constructor(public readonly c: string) {}
-
-//     do(): Message {
-//         return new Message(this.c)
-//     }
-// }
-
-class ToUpperCase implements Filter {
-    constructor(public readonly f: Filter) { }
-    next(): Message {
-       return new Message(this.f.next().value.toUpperCase())
-    }
-
-    hasNext(): Boolean {
-        return this.f.hasNext()
+    async pull(queue: AsyncQueue<T>): Promise<void> {
+        queue.dequeue().then(res => console.log(this.name + " " + res))
     }
 }
 
-class Writer implements Filter {
-    constructor(public readonly f: Filter) { }
-    next(): Message {
-        console.log(this.f.next().value.toString())
-        return Message.none
-    }
-
-    hasNext(): Boolean {
-        return this.f.hasNext()
+class AQPublisher<T> {
+    async push(queue: AsyncQueue<T>, arg: T): Promise<void> {
+        queue.enqueue(arg);
     }
 }
 
-class FileLineReader implements Filter {
-    lines: string[]
-    constructor(public readonly fileName: string) {
-        this.lines = readFileSync(fileName, 'utf-8').split('\n')
+class AsyncQueue<T> {
+    public queue: Array<T> = new Array()
+    protected queuedPromises: Array<any> = new Array()
+
+    async enqueue(arg: T): Promise<void> {
+        if(this.queuedPromises.length > 0)
+            this.queuedPromises.shift()(arg)
+        else
+            this.queue.push(arg)
     }
-
-    next(): Message {
-        return new Message(this.lines.shift())
-    }
-
-    hasNext(): Boolean {
-        return this.lines.length > 0
-    }
-}
-
-class SlowFileLineReader extends FileLineReader {
-    constructor(public readonly fileName: string) {
-        super(fileName)
-    }  
-
-    delay(millis: number) {
-        const date = new Date()
-        let curDate = null
-        do { 
-            curDate = new Date() 
-        } while (curDate.getTime() - date.getTime() < millis)
-    }
-
-    next(): Message {
-        this.delay(2000)
-        return new Message(this.lines.shift())
+    
+    async dequeue(): Promise<T> {
+        if(this.queue.length > 0)
+            return Promise.resolve(this.queue.shift())
+        else
+            return new Promise(res => this.queuedPromises.push(res))
     }
 }
 
-class Join implements Filter {
-    fs: Filter[]
-    currentFilter = 0
+let queue = new AsyncQueue()
+let p1 = new AQPublisher()
+let s1 = new AQSubscriber('s1')
+let s2 = new AQSubscriber('s2')
+let s3 = new AQSubscriber('s3')
 
-    constructor(...fs: Filter[]) { 
-        this.fs = fs
+s1.pull(queue)
+s2.pull(queue)
+s3.pull(queue)
+p1.push(queue,'ola')
+p1.push(queue,'ola')
+p1.push(queue,'ola')
+
+
+class AsyncSemaphore<T> {
+    private count: number = 0
+
+    signal(): void {
+        //se houver promises em espera, quando o count aumentar tem de a resolver
+        this.count++;
     }
 
-    next(): Message {
-        const f = this.fs[this.currentFilter]
-        this.currentFilter = (this.currentFilter + 1) % this.fs.length
-        if (f.hasNext()) return f.next()
-        else return this.next()
-    }
-
-    hasNext(): Boolean {
-        return this.fs.filter(f => f.hasNext()).length > 0
+    async wait(): Promise<void> {
+        //if (count == 0) tem de arranjar maneira de ficar à espera que o count aumente (promise)
+        //se já houver alguma promise em espera, não pode passar à frente das outras promises
+        this.count--;
     }
 }
-
-function iterate(f: Filter) {
-    while(f.hasNext()) { 
-        f.next()
-    }  
-}
-
-const f1 = new SlowFileLineReader('./best15.txt')
-const f2 = new FileLineReader('./best-mieic.txt')
-
-const r1 = new Writer(new ToUpperCase(new Join(f1, f2)))
-
-iterate(r1)
